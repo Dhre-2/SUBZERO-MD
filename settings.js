8f752c5322dab454306c84488cda11261e1e3bf1
Dhre-2 <jamon8537@gmail.com>
2025-07-25 20:09:16 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;iN0DhIQY#ktAp6l9Xx9YqFCEVo3eD1xCp0NB4QCpespVvYhIKqcs", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "2348031504740", // Put Owner Number Here 
+  PREFIX: "," // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
 // CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
